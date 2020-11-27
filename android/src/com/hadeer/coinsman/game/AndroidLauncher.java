91ce9a1b9c32c5b60aa6ed3cf0dd95bb971fb20c
hadeerKarem <hadeer.karem95@gmail.com>
2020-11-27 16:08:45 +0200

    Initial commit

---
+++ android/src/com/hadeer/coinsman/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.hadeer.coinsman.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.hadeer.coinsman.game.CoinsMan;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CoinsMan(), config);
+	}
+}
